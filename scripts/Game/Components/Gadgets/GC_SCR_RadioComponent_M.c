b113c754905b8dd9045c1ec2f9055befba8c42ab
Til-Weimann <67155539+Til-Weimann@users.noreply.github.com>
2026-01-14 00:25:14 +0100

    scripts

---
+++ scripts/Game/Components/Gadgets/GC_SCR_RadioComponent_M.c
@@ -0,0 +1,5 @@
+modded class SCR_RadioComponent
+{
+	[Attribute("0", UIWidgets.CheckBox, desc: "Whether the radio will automatically be tuned to the players group frequency. Tick then if the radio is intended to be used as a squad radio.", category: "Radio")]
+	bool m_bTuneToSquadFrequency;
+}
